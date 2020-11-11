2ca22109a06d00b1d0887fd7c6c2691bd36ec1f8
mihamed <mihamed@ntgclarity.com>
2020-11-11 08:38:14 +0200

    third

---
+++ lib/third.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Third extends StatefulWidget {
+  Third({Key key}) : super(key: key);
+
+  @override
+  _ThirdState createState() => _ThirdState();
+}
+
+class _ThirdState extends State<Third> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
